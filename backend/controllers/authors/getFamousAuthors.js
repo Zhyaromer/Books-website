74f476f3cf25b4fbcd8d42425293ea592da05511
Zhyaromer <zhyaraland123@gmail.com>
2025-02-25 00:53:10 +0300

    get famous authors endpoint is done

---
+++ backend/controllers/authors/getFamousAuthors.js
@@ -0,0 +1,26 @@
+const db = require('../../config/SQL/sqlconfig');
+const xss = require('xss');
+
+const getFamousAuthors = (req, res) => {
+    try {
+        const sql = `
+         SELECT id,name,imgURL FROM authors ORDER BY authors.views DESC LIMIT 6
+        `;
+
+        db.query(sql, (err, result) => {
+            if (err) {
+                return res.status(500).json({ message: 'Internal Server Error' });
+            }
+
+            if (result.length === 0) {
+                return res.status(404).json({ message: 'No author found' });
+            }
+
+            return res.status(200).json(result);
+        });
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+module.exports = getFamousAuthors;
--- backend/routes/authors/authorsRoutes.js
+++ backend/routes/authors/authorsRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const getAllAuthors = require('../../controllers/authors/getAllAuthors');
+const getFamousAuthors = require('../../controllers/authors/getFamousAuthors');
 
 router.get('/getallauthors', getAllAuthors);
+router.get('/getfamousauthors', getFamousAuthors);
 
 module.exports = router;
