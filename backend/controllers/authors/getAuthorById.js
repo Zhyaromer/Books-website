fbacb85726781cc986c7c1de1a8d761409bbc2f1
Zhyaromer <zhyaraland123@gmail.com>
2025-02-25 01:01:32 +0300

    get authors by id endpoint is done

---
+++ backend/controllers/authors/getAuthorById.js
@@ -0,0 +1,48 @@
+const db = require('../../config/SQL/sqlconfig');
+const xss = require('xss');
+
+const getAuthorById = async (req, res) => {
+    const { id } = req.params;
+    if (!id) {
+        return res.status(400).json({ message: 'Author ID is missing' });
+    }
+
+    const sanitizedId = id ? xss(id) : null;
+
+    try {
+        const sql1 = `
+        select * from authors where id = ?
+        `
+
+        const sql2 = `
+        select * from books where author_id = ?
+        `
+
+        db.query(sql1, [sanitizedId], (err, author) => {
+            if (err) {
+                return res.status(500).json({ message: 'Internal Server Error' });
+            }
+
+            if (author.length === 0) {
+                return res.status(404).json({ message: 'No author found' });
+            }
+
+            db.query(sql2, [sanitizedId], (err, books) => {
+                if (err) {
+                    return res.status(500).json({ message: 'Internal Server Error' });
+                }
+
+                if (books.length === 0) {
+                    return res.status(404).json({ message: 'No books found' });
+                }
+
+                return res.status(200).json({ author, books });
+            });
+        });
+
+    } catch (error) {
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+}
+
+module.exports = getAuthorById;
--- backend/routes/authors/authorsRoutes.js
+++ backend/routes/authors/authorsRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const getAllAuthors = require('../../controllers/authors/getAllAuthors');
 const getFamousAuthors = require('../../controllers/authors/getFamousAuthors');
+const getAuthorById = require('../../controllers/authors/getAuthorById');
 
 router.get('/getallauthors', getAllAuthors);
 router.get('/getfamousauthors', getFamousAuthors);
+router.get('/getAuthorById/:id', getAuthorById);
 
 module.exports = router;
