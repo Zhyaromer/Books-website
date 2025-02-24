3cb4b44508beaedeafab80cd8a5325899c016d85
Zhyaromer <zhyaraland123@gmail.com>
2025-02-24 22:42:58 +0300

    get trending books endpoint is done

--- backend/controllers/books/getTrendingBooks.js
+++ backend/controllers/books/getTrendingBooks.js
@@ -1,10 +1,32 @@
 const db = require('../../config/SQL/sqlconfig');
 const xss = require('xss');
 
-const getBooksMainPage = (req, res) => {
+const getTrendingBooks = (req, res) => {
     try {
-        
+        const sql = `
+        SELECT books.*, 
+        authors.name, authors.bio, authors.imgURL
+        FROM books
+        INNER JOIN authors ON books.author_id = authors.id
+        where books.created_at >= NOW() - INTERVAL 1 WEEK
+        ORDER BY COALESCE(books.views, 0) DESC
+        LIMIT 6
+        `
+        db.query(sql, (err, result) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: 'Internal Server Error' });
+            }
+
+            if (result.length === 0) {
+                return res.status(404).json({ message: 'No books found' });
+            }
+
+            return res.status(200).json(result);
+        });
     } catch (error) {
-        
+        return res.status(500).json({ message: 'Internal Server Error' });
     }
 }
+
+module.exports = getTrendingBooks;
--- backend/routes/books/booksRoutes.js
+++ backend/routes/books/booksRoutes.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const getAllBooks = require('../../controllers/books/getAllBooks');
 const getBookById = require('../../controllers/books/getBookById');
 const getBooksMainPage = require('../../controllers/books/getBooksMainPage');
+const getTrendingBooks = require('../../controllers/books/getTrendingBooks');
 
 router.get('/getAllBooks', getAllBooks);
 router.get('/getBookById/:id', getBookById);
 router.get('/getBooksMainPage', getBooksMainPage);
+router.get('/getTrendingBooks', getTrendingBooks);
 
 module.exports = router;
